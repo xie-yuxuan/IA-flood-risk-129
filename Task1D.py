b51d954dd89f290ee04c1cd4e7bfb84159700c20
Xie Yuxuan <yx353@cam.ac.uk>
2022-02-09 12:47:57 +0000

    sort list in alphabetical order

--- Task1D.py
+++ Task1D.py
@@ -5,7 +5,7 @@ from floodsystem.utils import sorted_by_key  # noqa
 from floodsystem.geo import stations_by_river, rivers_with_station
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1D"""
 
     #part a
     # Build list of stations
@@ -14,12 +14,8 @@ def run():
     #list of rivers with at least one station
     r_list = list(rivers_with_station(stations))
 
-    #number of the rivers
-    print(len(r_list))
-
-    #the names of first the rivers
-    print(r_list[:10])
-
+    #print number of stations and first 10 stations
+    print(str(len(r_list)) + " stations. First 10 - " + str(sorted(r_list)[:10]))
 
     #part b
     #dictionary of rivers with their monitoring stations
